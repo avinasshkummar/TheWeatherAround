045aa0ddb9507ffaebaa28007e921ec6937915c8
avinasshkummar <avinasshkummar@gmail.com>
2021-04-19 17:21:47 +0530

    Add project files.

---
+++ TheWeatherAround/Controllers/HomeController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TheWeatherAround.Models;
+
+namespace TheWeatherAround.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Here We help you know the weather of all the places you wish to know";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Contact me.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult WeatherDetail(string City)
+        {
+
+            //Assign API KEY which received from OPENWEATHERMAP.ORG  
+            string appId = "2c764384532120a441275cd0bc63bf69";
+
+            //API path with CITY parameter and other parameters.  
+            string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&units=metric&cnt=1&APPID={1}", City, appId);
+
+            using (WebClient client = new WebClient())
+            {
+                string json = string.Empty;
+                RootObject weatherInfo = new RootObject();
+                try
+                {
+                    json=client.DownloadString(url);
+                    //Converting to OBJECT from JSON string.  
+                    weatherInfo = (new System.Web.Script.Serialization.JavaScriptSerializer()).Deserialize<RootObject>(json);
+                }
+                catch (Exception)
+                {
+                    return Json(new { status = "error" });
+                }
+                
+
+                //********************//  
+                //     JSON RECIVED   
+                //********************//  
+                //{"coord":{ "lon":72.85,"lat":19.01},  
+                //"weather":[{"id":711,"main":"Smoke","description":"smoke","icon":"50d"}],  
+                //"base":"stations",  
+                //"main":{"temp":31.75,"feels_like":31.51,"temp_min":31,"temp_max":32.22,"pressure":1014,"humidity":43},  
+                //"visibility":2500,  
+                //"wind":{"speed":4.1,"deg":140},  
+                //"clouds":{"all":0},  
+                //"dt":1578730750,  
+                //"sys":{"type":1,"id":9052,"country":"IN","sunrise":1578707041,"sunset":1578746875},  
+                //"timezone":19800,  
+                //"id":1275339,  
+                //"name":"Mumbai",  
+                //"cod":200}  
+
+               
+
+                //Special VIEWMODEL design to send only required fields not all fields which received from   
+                //www.openweathermap.org api  
+                WeatherViewModel rslt = new WeatherViewModel();
+
+                rslt.Country = weatherInfo.sys.country;
+                rslt.City = weatherInfo.name;
+                rslt.Lat = Convert.ToString(weatherInfo.coord.lat);
+                rslt.Lon = Convert.ToString(weatherInfo.coord.lon);
+                rslt.Description = weatherInfo.weather[0].description;
+                rslt.Humidity = Convert.ToString(weatherInfo.main.humidity);
+                rslt.Temp = Convert.ToString(weatherInfo.main.temp);
+                rslt.TempFeelsLike = Convert.ToString(weatherInfo.main.feels_like);
+                rslt.TempMax = Convert.ToString(weatherInfo.main.temp_max);
+                rslt.TempMin = Convert.ToString(weatherInfo.main.temp_min);
+                rslt.WeatherIcon = weatherInfo.weather[0].icon;
+
+                //Converting OBJECT to JSON String   
+                var jsonstring = new System.Web.Script.Serialization.JavaScriptSerializer().Serialize(rslt);
+
+                //Return JSON string. 
+                return Json(new { Weather = jsonstring });
+            }
+        }
+
+    }
+
+}
